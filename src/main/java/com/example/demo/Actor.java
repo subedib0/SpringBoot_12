abb500adf14cf06da302c434da3670a37bc0fbc3
subedib0 <subedib0@gmail.com>
2018-02-12 17:28:46 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/Actor.java
@@ -0,0 +1,59 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Actor {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private long id;
+
+
+    private String name;
+
+    private String realname;
+
+    private String headshot;
+
+
+    public Actor() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRealname() {
+        return realname;
+    }
+
+    public void setRealname(String realname) {
+        this.realname = realname;
+    }
+
+    public String getHeadshot() {
+        return headshot;
+    }
+
+    public void setHeadshot(String headshot) {
+        this.headshot = headshot;
+    }
+}
---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,54 @@
+package com.example.demo;
+
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Map;
+
+@Controller
+public class HomeController {
+
+    ActorRepository actorRepository;
+    @Autowired
+    CloudinaryConfig cloudc;
+
+    @RequestMapping("/" )
+    public String listActors(Model model) {
+        model.addAttribute("actors", actorRepository.findAll());
+        return "list";
+
+    }
+
+    @GetMapping("/add" )
+    public String newtActors(Model model) {
+        model.addAttribute("actor", new Actor());
+        return "list";
+    }
+
+    @PostMapping("/add" )
+    public String processActor(@ModelAttribute Actor actor, @RequestParam("file" ) MultipartFile file) {
+
+        if (file.isEmpty()) {
+            return "redirect:/add";
+
+        }
+
+
+        try {
+            Map uploadResult = cloudc.upload(file.getBytes(), ObjectUtils.asMap("resourcetype", "auto" ));
+            actor.setHeadshot(uploadResult.get("url" ).toString());
+            actorRepository.save(actor);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "redirect:/add";
+        }
+
+        return "redirect:/";
+
+    }
+}
